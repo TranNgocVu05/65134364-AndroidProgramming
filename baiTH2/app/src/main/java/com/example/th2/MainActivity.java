8a1d15324c5c811d85bbb65f0d61acf659e4c1d1
Ngọc vũ <147695859+TranNgocVu05@users.noreply.github.com>
2026-03-12 19:20:44 +0700

    BaiTH2_2

--- baiTH2/app/src/main/java/com/example/th2/MainActivity.java
+++ baiTH2/app/src/main/java/com/example/th2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.th2;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // day la bo lang nghe va xu ky click len nut button
+    public void XuLyCong(View view ){
+        // tim cac tham so
+        EditText  editTextSoA = findViewById(R.id.edtA);
+        EditText  editTextSoB = findViewById(R.id.edtB);
+        EditText  editTextKQ = findViewById(R.id.edtKq);
+        // lay du lieu
+        String strA = editTextSoA.getText().toString();
+        // lay du lieu
+        String strB = editTextSoB.getText().toString();
+        // dua du lieu sang dang so
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+        // tinh  tong
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+        // hien ra man hinh
+        editTextKQ.setText(strTong);
+
+
+
     }
 }
