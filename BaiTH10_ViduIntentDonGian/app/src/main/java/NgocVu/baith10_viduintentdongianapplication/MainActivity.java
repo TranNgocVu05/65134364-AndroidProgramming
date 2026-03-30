679673ea996a4b14f3f5985ad3e8ecf771e57739
Ngọc vũ <147695859+TranNgocVu05@users.noreply.github.com>
2026-03-30 22:23:54 +0700

    BaiTH10_ViduIntentDonGian
    BaiTH10_ViduIntentDonGian

---
+++ BaiTH10_ViduIntentDonGian/app/src/main/java/NgocVu/baith10_viduintentdongianapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package NgocVu.baith10_viduintentdongianapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void OnTab(View v){
+        Intent iManHinhKhac = new Intent(this, ActivityOne.class);
+        startActivity(iManHinhKhac);
+    }
+}
