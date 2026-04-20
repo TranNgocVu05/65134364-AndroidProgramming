1b7da3cdc4f3e0462ddb409f97625e3aa8b4fd59
Ngọc vũ <147695859+TranNgocVu05@users.noreply.github.com>
2026-04-20 08:20:22 +0700

    thi giữa kì 2
    thi giữa kì 2

--- ThiGk2/app/src/main/java/thigk2/tranngocvu/thigk2/Cau3Fragment.java
+++ ThiGk2/app/src/main/java/thigk2/tranngocvu/thigk2/Cau3Fragment.java
@@ -26,11 +26,11 @@ public class Cau3Fragment extends Fragment {
 
         List<DiaDiem> dsDiaDiem = new ArrayList<>();
         // Đã đổi thành R.mipmap.thaptramhuong
-        dsDiaDiem.add(new DiaDiem("Tháp Bà Ponagar", "2 Thắng 4, Vĩnh Phước, Nha Trang", R.mipmap.thaptramhuong));
-        dsDiaDiem.add(new DiaDiem("Chùa Long Sơn", "20 Đường 23/10, Phương Sơn, Nha Trang", R.mipmap.thaptramhuong));
-        dsDiaDiem.add(new DiaDiem("VinWonders Nha Trang", "Đảo Hòn Tre, Nha Trang", R.mipmap.thaptramhuong));
-        dsDiaDiem.add(new DiaDiem("Viện Hải dương học", "01 Cầu Đá, Nha Trang", R.mipmap.thaptramhuong));
-        dsDiaDiem.add(new DiaDiem("Hòn Chồng", "Vĩnh Phước, Nha Trang", R.mipmap.thaptramhuong));
+        dsDiaDiem.add(new DiaDiem("Tháp Bà Ponagar", "2 Thắng 4, Vĩnh Phước, Nha Trang", R.mipmap.thapba));
+        dsDiaDiem.add(new DiaDiem("Chùa Long Sơn", "20 Đường 23/10, Phương Sơn, Nha Trang", R.mipmap.chualongson));
+        dsDiaDiem.add(new DiaDiem("VinWonders Nha Trang", "Đảo Hòn Tre, Nha Trang", R.mipmap.vinpear));
+        dsDiaDiem.add(new DiaDiem("Viện Hải dương học", "01 Cầu Đá, Nha Trang", R.mipmap.vienhaiduong));
+        dsDiaDiem.add(new DiaDiem("Hòn Chồng", "Vĩnh Phước, Nha Trang", R.mipmap.honchong));
 
         DiaDiemAdapter adapter = new DiaDiemAdapter(dsDiaDiem);
         recyclerView.setAdapter(adapter);
